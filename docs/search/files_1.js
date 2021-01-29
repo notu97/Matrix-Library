8ef91c50ed23a0dbcd3aa136ff3fda74707a27ec
Shiladitya Biswas <shiladitya97@outlook.com>
2021-01-28 16:50:35 -0800

    Readme Updated

--- docs/search/files_1.js
+++ docs/search/files_1.js
@@ -1,6 +1,5 @@
 var searchData=
 [
   ['matrix_2ecpp',['matrix.cpp',['../d5/d84/matrix_8cpp.html',1,'']]],
-  ['matrix_2ed',['matrix.d',['../d5/d23/matrix_8d.html',1,'']]],
   ['matrix_2eh',['matrix.h',['../dd/df4/matrix_8h.html',1,'']]]
 ];
